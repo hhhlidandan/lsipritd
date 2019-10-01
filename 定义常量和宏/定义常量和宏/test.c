b9618e304791723008644d50bef0bcd584372121
hhhlidandan <lsipritd1998@163.com>
2019-10-01 20:37:05 +0800

    定义宏和常量

---
+++ 定义常量和宏/定义常量和宏/test.c
@@ -0,0 +1,20 @@
+//#include<stdio.h>
+//#define MAX 10 // MAX为常量
+//int main()
+//{
+//	printf("%d ", MAX);//10
+//		return 0;
+//}
+
+
+//#include<stdio.h>
+//
+//#define ADD(x,y)((x)+(y)) //x,y可能也会是表达式，所以必须得有（）
+//
+//int main()
+//{
+//	int sum = ADD(2, 3);
+//	printf("sum = %d\n", sum);// sum = 5
+//
+//	return 0;
+//}
