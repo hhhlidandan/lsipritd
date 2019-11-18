2329912236c12ee5545bdf95bb19f27a7e87785e
hhhlidandan <lsipritd1998@163.com>
2019-11-18 20:47:40 +0800

    const

---
+++ 博客-const/博客-const/test.c
@@ -0,0 +1,64 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<windows.h>
+#pragma warning(disable:4996)
+
+//int main()
+//{
+//
+//	char str[] = "hello world";
+//	char str1[] = "hello world";
+//	char *str2 = "hello world";
+//	char *str3 = "hello world";
+//	if (str == str1)
+//	{
+//		printf("1same\n");
+//	}
+//	else
+//	{
+//		printf("1different\n");
+//	}
+//	if (str2 == str3)
+//	{
+//		printf("2same\n");
+//	}
+//	else
+//	{
+//		printf("2different\n");
+//	}
+//	/*char ch = 'a';
+//	char *p = &ch;
+//	char *str = "abcdef";*/
+//	system("pause");
+//	return 0;
+//}
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	const int *p1= &a;//const修饰*，*p（指针指向的内容）不能直接被修改
+//	int const *p1= &a;//和上面一样
+//	*p1 = 30;
+//	p1 = b;
+//
+//	int *const p = &a;//修饰变量p,表示代表p的指向不能变化
+//	*p = 30;//*p是a,所以能被改
+//	p = 20;//const修饰p，不能被改.
+//
+//	const int *const p2 = 20;//第一个const修饰*，第二个const修饰p2,表示const
+//	//const修饰指针，指针指向的目标的内容不能改变
+//	//修饰变量，代表p不能被改（p的指向不能被改）
+//
+//	const int a1 = 10;
+//	const int *p7 = &a;//为了保证a不会被间接修改，所以前面加const
+//
+//	a1 = 100;
+//	*p7 = 200;
+//
+//	const char *str = "abcdef";
+//
+//	system("pause");
+//	return 0;
+//
+//}
+
