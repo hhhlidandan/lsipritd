a0586d59995240399bc283bd7b0aa75f3b63c2ca
hhhlidandan <lsipritd1998@163.com>
2019-11-26 17:50:34 +0800

    11111

---
+++ 11-22MRYT/11-22MRYT/test.cpp
@@ -0,0 +1,39 @@
+//
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	int n;
+//	while (cin >> n)
+//	{
+//		int count = 0;
+//		while (n)
+//		{
+//			if (n & 1)
+//			{
+//				count++;
+//			}
+//			n >>= 1;
+//		}
+//		cout << count << endl;
+//	}
+//	return 0;
+//}
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	while (cin >> n)
+	{
+		int count = 0;
+		while (n)
+		{
+			n &= (n - 1);
+			count++;
+		}
+		cout << count << endl;
+	}
+	return 0;
+}
