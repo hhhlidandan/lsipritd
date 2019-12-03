7d3d0efd654604b4c5982d3c2d52465bc3fe5059
hhhlidandan <lsipritd1998@163.com>
2019-12-03 21:09:49 +0800

    微信红包

---
+++ 12-1MRYT/12-1MRYT/test.cpp
@@ -0,0 +1,18 @@
+//微信红包
+//class Gift {
+//public:
+//	int getValue(vector<int> gifts, int n) {
+//		sort(gifts.begin(), gifts.end());
+//		int count = 0;
+//		for (int i = 0; i<n; i++)
+//		{
+//			if (gifts[i] == gifts[n / 2])
+//				count++;
+//		}
+//		if (count>n / 2)
+//			return gifts[n / 2];
+//		return 0;
+//		// write code here
+//	}
+//};
+//字符串距离  （不会）
