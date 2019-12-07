621018b2fcb4a4d1e02e1f79321540ab8f771f62
hhhlidandan <lsipritd1998@163.com>
2019-12-07 20:57:22 +0800

    青蛙跳台阶  快到碗里来

---
+++ 12-5MRYT 青蛙跳台阶    快到碗里来/12-5MRYT 青蛙跳台阶    快到碗里来/test.cpp
@@ -0,0 +1,27 @@
+//青蛙跳台阶
+//class Solution {
+//public:
+//	int jumpFloorII(int number) {
+//		return 1 << (number - 1);
+//
+//	}
+//};
+
+//快到碗里来
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	double cat_l;
+//	double bowl_r;
+//	while (cin >> cat_l >> bowl_r)
+//	{
+//		double bowl_l = 2 * 3.14*bowl_r;
+//		if (bowl_l >= cat_l)
+//			cout << "Yes" << endl;
+//		else{
+//			cout << "No" << endl;
+//		}
+//	}
+//	return 0;
+//}
