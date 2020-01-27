0ceb075d8115ca1b00062278bf725b784b0c0ca1
hhhlidandan <lsipritd1998@163.com>
2020-01-27 19:31:34 +0800

    this指针

--- 类与对象2/类与对象2/test.h
+++ 类与对象2/类与对象2/test.h
@@ -5,6 +5,17 @@
 #include<iostream>
 #pragma warning(disable:4996)
 using namespace std;
+class Student
+{
+public:
+	void InitStudent(char* name, char* gender, int sex);
+	void PrintStudent();
+private:
+	char _name[20];
+	char _gender[5];
+	int _age;
+};
+#if 1
 class A
 {
 public:
@@ -21,3 +32,4 @@ public:
 class C
 {
 };
+#endif
