0dcbbf8ab02698da82f1f22e9b7cfd677aa49e1e
hhhlidandan <lsipritd1998@163.com>
2020-03-19 23:22:51 +0800

    用递归、非递归方式反转链表

--- 链表面试题--leetcode--反转链表/链表面试题--leetcode--反转链表/test.cpp
+++ 链表面试题--leetcode--反转链表/链表面试题--leetcode--反转链表/test.cpp
@@ -18,7 +18,8 @@
 class Solution {
 public:
 	ListNode* reverseList(ListNode* head) {
-		if (head == NULL)  return 0;
+		//三个指针法
+		/*if (head == NULL)  return 0;
 		ListNode* p = head->next;
 		ListNode* Cur = head;
 		head->next = NULL;
@@ -29,6 +30,45 @@ public:
 			p = head->next;
 			head->next = Cur;
 		}
-		return head;
+		return head;*/
+
+		//递归
+		//输入：1->2->3->4->5->NULL
+		//输出 : 5->4->3->2->1->NULL
+		if (head)
+		{
+			reverseList(head->next);
+			printf("%d", head->val);
+		}
 	}
 };
+//三个指针
+class Solution {
+public:
+	ListNode* reverseList(ListNode* head) {
+		if (head == NULL) return NULL;
+		ListNode* p = NULL;
+		ListNode* Cur = head->next;
+		while (head)
+		{
+			head->next = p;
+			p = head;
+			head = Cur;
+			if (head) Cur = head->next;
+		}
+		return p;
+	}
+};
+//用递归方式打印    遵循后调用先退出
+class Solution {
+public:
+	ListNode* reverseList(ListNode* head) {
+		if (head == NULL || head->next == NULL)
+			return head;
+		ListNode* newhead = reverseList(head->next);
+		head->next->next = head;
+		head->next = NULL;
+		return newhead;
+	}
+};
+//将递归转成循环  有些代码可以直接转化为循环，有些递归需要借助栈转化为循环
