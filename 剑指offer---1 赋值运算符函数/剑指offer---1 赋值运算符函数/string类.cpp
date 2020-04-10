2f366b440a7bf522d5ade04529b16a103854234e
hhhlidandan <lsipritd1998@163.com>
2020-04-10 22:18:32 +0800

    构造、拷贝构造、 赋值运算符重载

--- 剑指offer---1 赋值运算符函数/剑指offer---1 赋值运算符函数/string类.cpp
+++ 剑指offer---1 赋值运算符函数/剑指offer---1 赋值运算符函数/string类.cpp
@@ -1,70 +1,70 @@
-#include<iostream>
-using namespace std;
-namespace bit
-{
-	class string
-	{
-	public:
-		//构造
-		string(const char* str)
-		{
-			if (str == nullptr)
-				str = "";
-			_size = strlen(str) + 1;
-			_capacity = _size;
-			_str = new char[strlen(str) + 1];
-
-			strcpy(_str, str);
-		}
-		string(char c, int n)
-			:_str(new char[n + 1]),
-			_size(n),
-			_capacity(_size)
-		{
-			memset(_str, c, _size);
-		}
-		string(char* pstr,int n)//前n个字符
-		{
-			_size = strlen(pstr) > n ? n : strlen(pstr);
-			_capacity = _size;
-			_str = new char[_capacity + 1];
-			for (int i = 0; i < _size; i++)
-				_str[i] = pstr[i];
-			_str[_size] = '\0';
-		}
-		//拷贝构造
-		string(const string& s) :_str(new char[strlen(_str)+1])
-		{
-			strcpy(_str, s._str);
-		}
-		//赋值运算符重载
-		string& operator=(const string& s)
-		{
-			if (this != &s)//判断是否自己给自己赋值
-			{
-				char *ptr = new char[strlen(s._str) + 1];
-				strcpy(ptr, s._str);
-				delete[] _str;//释放之前已有的
-				_str = ptr;
-			}
-			return *this;
-		}
-		//析构函数
-		~string()
-		{
-			if (_str)
-			{
-				delete[] _str;
-				_str == nullptr;
-			}
-		}
-	private:
-		char* _str;
-		int _size;
-		int _capacity;
-	};
-}
-int main()
-{
-	return 0;
-}
+//#include<iostream>
+//using namespace std;
+//namespace bit
+//{
+//	class string
+//	{
+//	public:
+//		//构造
+//		string(const char* str)
+//		{
+//			if (str == nullptr)
+//				str = "";
+//			_size = strlen(str) + 1;
+//			_capacity = _size;
+//			_str = new char[strlen(str) + 1];
+//
+//			strcpy(_str, str);
+//		}
+//		string(char c, int n)
+//			:_str(new char[n + 1]),
+//			_size(n),
+//			_capacity(_size)
+//		{
+//			memset(_str, c, _size);
+//		}
+//		string(char* pstr,int n)//前n个字符
+//		{
+//			_size = strlen(pstr) > n ? n : strlen(pstr);
+//			_capacity = _size;
+//			_str = new char[_capacity + 1];
+//			for (int i = 0; i < _size; i++)
+//				_str[i] = pstr[i];
+//			_str[_size] = '\0';
+//		}
+//		//拷贝构造
+//		string(const string& s) :_str(new char[strlen(_str)+1])
+//		{
+//			strcpy(_str, s._str);
+//		}
+//		//赋值运算符重载
+//		string& operator=(const string& s)
+//		{
+//			if (this != &s)//判断是否自己给自己赋值
+//			{
+//				char *ptr = new char[strlen(s._str) + 1];
+//				strcpy(ptr, s._str);
+//				delete[] _str;//释放之前已有的
+//				_str = ptr;
+//			}
+//			return *this;
+//		}
+//		//析构函数
+//		~string()
+//		{
+//			if (_str)
+//			{
+//				delete[] _str;
+//				_str == nullptr;
+//			}
+//		}
+//	private:
+//		char* _str;
+//		int _size;
+//		int _capacity;
+//	};
+//}
+//int main()
+//{
+//	return 0;
+//}
--- 剑指offer---1 赋值运算符函数/剑指offer---1 赋值运算符函数/test.cpp
+++ 剑指offer---1 赋值运算符函数/剑指offer---1 赋值运算符函数/test.cpp
@@ -1,42 +1,42 @@
-#include<iostream>
-using namespace std;
-class CMyString
-{
-public:
-	CMyString(char* pData = NULL);
-	CMyString(const char& str);
-	CMyString& operator=(const CMyString& str)
-	{
-		if (this!=&str)
-		{
-			//1.可用（传统版深拷贝）
-			char* ptr = new char[strlen(str.m_pData) + 1];//1.先申请，有异常会抛异常，后面不会进行，原来的实例没有改，保证了异常安全性
-			strcpy(ptr, str.m_pData);
-			delete[] m_pData;//2.再释放自己之前的
-			m_pData = NULL;
-			m_pData = ptr;
-
-			//2.不建议这样先释放，如果申请空间失败，那m_pData会是一个空指针，可能会导致程序崩溃
-			//delete[] m_pData;
-			//m_pData = nullptr;
-			//m_pData = new char[strlen(str.m_pData) + 1];
-			//strcpy(m_pData, str.m_pData);
-
-			////3.可用 （现代版深拷贝）
-			//CMyString temp(str);
-			////swap(m_pData, temp.m_pData);
-			//char* t = temp.m_pData;
-			//temp.m_pData = m_pData;
-			//m_pData = t;
-		}
-		return *this;
-	}
-	~CMyString(void);
-private:
-	char* m_pData;
-};
-int main()
-{
-
-	return 0;
-}
+//#include<iostream>
+//using namespace std;
+//class CMyString
+//{
+//public:
+//	CMyString(char* pData = NULL);
+//	CMyString(const char& str);
+//	CMyString& operator=(const CMyString& str)
+//	{
+//		if (this!=&str)
+//		{
+//			//1.可用（传统版深拷贝）
+//			char* ptr = new char[strlen(str.m_pData) + 1];//1.先申请，有异常会抛异常，后面不会进行，原来的实例没有改，保证了异常安全性
+//			strcpy(ptr, str.m_pData);
+//			delete[] m_pData;//2.再释放自己之前的
+//			m_pData = NULL;
+//			m_pData = ptr;
+//
+//			//2.不建议这样先释放，如果申请空间失败，那m_pData会是一个空指针，可能会导致程序崩溃
+//			//delete[] m_pData;
+//			//m_pData = nullptr;
+//			//m_pData = new char[strlen(str.m_pData) + 1];
+//			//strcpy(m_pData, str.m_pData);
+//
+//			////3.可用 （现代版深拷贝）
+//			//CMyString temp(str);
+//			////swap(m_pData, temp.m_pData);
+//			//char* t = temp.m_pData;
+//			//temp.m_pData = m_pData;
+//			//m_pData = t;
+//		}
+//		return *this;
+//	}
+//	~CMyString(void);
+//private:
+//	char* m_pData;
+//};
+//int main()
+//{
+//
+//	return 0;
+//}
