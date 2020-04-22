2285ceacc288025a28e6dbddd24fe92d0435477c
hhhlidandan <lsipritd1998@163.com>
2020-04-22 21:52:09 +0800

    关于链表的题

---
+++ 剑指offer----关于链表的题/剑指offer----关于链表的题/反转单链表.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+struct ListNode {
+	int val;
+	struct ListNode *next;
+	ListNode(int x) :
+		val(x), next(NULL) {
+	}
+};
+class Solution {
+public:
+	ListNode* ReverseList(ListNode* pHead) {
+		if (pHead == NULL || pHead->next == NULL) return pHead;
+		ListNode* New = ReverseList(pHead->next);
+		pHead->next->next = pHead;
+		pHead->next = NULL;
+		return New;
+	}
+};
