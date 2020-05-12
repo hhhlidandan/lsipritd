19e1303efc024265918a082a42ad3f62cd0386bf
hhhlidandan <lsipritd1998@163.com>
2020-05-12 23:10:57 +0800

    字符串转整数

--- day06字符串转整数、不要二/day06字符串转整数、不要二/字符串转整型.cpp
+++ day06字符串转整数、不要二/day06字符串转整数、不要二/字符串转整型.cpp
@@ -5,18 +5,23 @@ using namespace std;
 int main()
 {
 	string str;
+	long long ret;
+	int flag = 1;
 	cin >> str;
 	int i = 0;
-	if (str[0] == '+' || str[0] == '-')
+	if (str[0] == '+') flag = 1;
+	else if (str[0] == '-') flag = -1;
+	else if (str[0] >= '0 '&& str[0] <= '9 ')
+		ret = str[0] - '0';
+
+	for (i = 1; i < str.size(); i++)
 	{
-		for (i = 1; i < str.size(); i++)
-		{
-			if (str[i] < '0'&&str[i] >= '9')
-				return 0;
-		}
-		return str;
+		if (str[i] >= '0' && str[i] <= '9')
+			ret = ret * 10 + str[i] - '0';
+		else return 0;
 	}
-	return 0;
+
+	return ret;
 	system("pause");
 	return 0;
 }
