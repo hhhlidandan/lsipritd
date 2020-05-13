870cb5f4a247bfe711c9b4480711893508becae4
hhhlidandan <lsipritd1998@163.com>
2020-05-13 23:18:14 +0800

    字符串转整数

--- day06字符串转整数、不要二/day06字符串转整数、不要二/字符串转整型.cpp
+++ day06字符串转整数、不要二/day06字符串转整数、不要二/字符串转整型.cpp
@@ -2,26 +2,38 @@
 #include<stdlib.h>
 #include<string>
 using namespace std;
-int main()
+int Ret(string &str)
 {
-	string str;
-	long long ret;
-	int flag = 1;
+	long long ret = 0;//
+	int flag = 0;//记录整数的正负
 	cin >> str;
-	int i = 0;
-	if (str[0] == '+') flag = 1;
+	if (str.empty()) return ret;
+	else if (str[0] == '+') flag = 1;
 	else if (str[0] == '-') flag = -1;
 	else if (str[0] >= '0 '&& str[0] <= '9 ')
-		ret = str[0] - '0';
+		ret = str[0] - '0';//数字字符转数字
+	else
+		return ret;
 
+	int i = 0;
 	for (i = 1; i < str.size(); i++)
 	{
 		if (str[i] >= '0' && str[i] <= '9')
 			ret = ret * 10 + str[i] - '0';
 		else return 0;
 	}
-
-	return ret;
+	if (flag >= 0 && ret <= INT_MAX) return ret;
+	else if (flag<0)
+	{
+		ret *= flag;
+		if (ret >= INT_MIN) return ret;
+	}
+}
+int main()
+{
+	string str;
+	int ret = Ret(str);
+	cout << ret << endl;
 	system("pause");
 	return 0;
 }
